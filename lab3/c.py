0ce5c6ef07e77ca87ec44e3d558f3f92970a687f
airinwq <air_suleimenova@kbtu.kz>
2025-01-29 10:14:39 +0500

    practice

--- lab3/c.py
+++ lab3/c.py
@@ -1,47 +1,138 @@
-#functions
+#functuions1
+#1
 
-def my_function():
-  print("Hello from a function")
+def grams_to_ounces(grams):
+    ounces = 28.3495231 * grams
+    return ounces
 
-my_function()
+grams = float(input("Введите количество граммов: "))
+print(grams, "граммов =", round(grams_to_ounces(grams), 2), "унций")
+
+#2
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+
+fahrenheit = float(input("Введите температуру в Фаренгейтах: "))
+print(fahrenheit, "Фаренгейт =", round(fahrenheit_to_celsius(fahrenheit), 2), "Цельсий")
 
 
-def my_function(fname):
-  print(fname + " Refsnes")
+#3
 
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+def solve(numheads, numlegs):
+    for chickens in range(numheads + 1):
+        rabbits = numheads - chickens
+        if (chickens * 2 + rabbits * 4) == numlegs:
+            return chickens, rabbits
+    return None
 
-#lambda
+numheads = 35
+numlegs = 94
+result = solve(numheads, numlegs)
+if result:
+    print("Курицы:", result[0], "Кролики:", result[1])
+else:
+    print("Решение не найдено")
 
-x = lambda a : a + 10
-print(x(5))
 
-x = lambda a, b : a * b
-print(x(5, 6))
+#functions2
 
-#classes?objects
+#1
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+def is_high_rated(movie):
+    return movie.get('imdb', 0) > 5.5
 
-p1 = Person("John", 36)
+movie = {'title': 'Inception', 'imdb': 8.8}
+print(is_high_rated(movie))
 
-print(p1.name)
-print(p1.age)
+#2
 
-#Inheritance
+def is_high_rated(movie):
+    return movie.get('imdb', 0) > 5.5
 
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+def filter_high_rated(movies):
+    return [movie for movie in movies if is_high_rated(movie)]
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+movies = [
+    {'title': 'Inception', 'imdb': 8.8},
+    {'title': 'Movie A', 'imdb': 5.0},
+    {'title': 'Movie B', 'imdb': 6.1}
+]
 
-x = Person("John", "Doe")
-x.printname()
+print(filter_high_rated(movies))
+
+#3
+
+def is_high_rated(movie):
+    return movie.get('imdb', 0) > 5.5
+
+def filter_high_rated(movies):
+    return [movie for movie in movies if is_high_rated(movie)]
+
+def filter_by_category(movies, category):
+    return [movie for movie in movies if movie.get('category') == category]
+
+movies = [
+    {'title': 'Inception', 'imdb': 8.8, 'category': 'Sci-Fi'},
+    {'title': 'Movie A', 'imdb': 5.0, 'category': 'Drama'},
+    {'title': 'Movie B', 'imdb': 6.1, 'category': 'Sci-Fi'}
+]
+
+print(filter_high_rated(movies))
+print(filter_by_category(movies, 'Sci-Fi'))
+
+#classes
+
+#1
+
+class StringProcessor:
+    def __init__(self):
+        self.string = ""
+    
+    def getString(self):
+        self.string = input("Введите строку: ")
+    
+    def printString(self):
+        print(self.string.upper())
+
+processor = StringProcessor()
+processor.getString()
+processor.printString()
+
+#2
+
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+    
+    def area(self):
+        return self.length * self.length
+
+square = Square(5)
+print("Площадь квадрата:", square.area())
+
+#3
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+
+
